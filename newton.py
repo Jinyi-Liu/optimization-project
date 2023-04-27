a512f8701e326441128e0ed59eed8d9b29fd81d0
Jinyi Liu <jinyi.liu99@gmail.com>
2023-04-27 15:09:48 -0400

    modified:   Newton.ipynb 	modified:   barrier.ipynb 	modified:   funcs.py 	modified:   hw4.ipynb 	modified:   newton-LP-EQ.ipynb 	modified:   newton.png 	modified:   newton.py 	modified:   pd-LP.ipynb 	modified:   pd-LP_INEQ.ipynb 	modified:   primal-dual.ipynb 	modified:   primi-dual-LP copy.ipynb 	modified:   primi-dual-LP.ipynb 	modified:   quasi-newton.ipynb 	modified:   test.py 	modified:   testbarrier.py 	modified:   testprimal.py

--- newton.py
+++ newton.py
@@ -28,30 +28,66 @@ def newton_eq(f, grad_f, nabla_f, x0, A, b, MAXITERS=100, TOL=1e-8,alpha = 0.01,
         print("Iteration: %d, decrement: %f" % (iters, decrement_value))
     return x
 
-def newton(f, grad_f, nabla_f, x0, A, b, MAXITERS=100, TOL=1e-8,alpha = 0.01, beta = 0.8):
+def newton(f, grad_f, nabla_f, x0, A, b, domf, MAXITERS=100, TOL=1e-8,alpha = 0.01, beta = 0.8):
     MAXITERS = MAXITERS
     TOL = TOL
     alpha = alpha
     beta = beta
-    f, grad_f, nabla_f = f, grad_f, nabla_f
+    f, grad_f, nabla_f, domf = f, grad_f, nabla_f, domf
     A, b = A, b
     m, n = A.shape
     x = x0
     decrement = lambda dx, x: (dx.dot(nabla_f(x).dot(dx)))
-    
+    decrement_value_list = []
+    x_list = [x0]
     for iters in range(MAXITERS):
         dx = np.linalg.solve(nabla_f(x), -grad_f(x))
         dx = dx[:n]
         decrement_value = decrement(dx, x)/2
+        decrement_value_list.append(decrement_value)
         if decrement_value < TOL:
+            print("Iteration: %d, decrement: %.10f" % (iters, decrement_value))
             break
         t = 1
-        while np.any(A@(x + t*dx) - b>0):
+        while not domf(x + t*dx):
+            print("t: %f" % t)
+            t *= beta
+
+        while f(x + t*dx) > f(x) - alpha * t * decrement_value:
+            t *= beta
+
+        x += t*dx
+        x_list.append(x)
+        # print("Iteration: %d, decrement: %.10f" % (iters, decrement_value))
+    return x
+
+def gradient_descent(f, grad_f, x0, A, b, domf, MAXITERS=100, TOL=1e-8,alpha=0.01, beta=0.8):
+    MAXITERS = MAXITERS
+    TOL = TOL
+    alpha = alpha
+    beta = beta
+    f, grad_f, domf = f, grad_f, domf
+    A, b = A, b
+    m, n = A.shape
+    x = x0
+    decrement = lambda  dx: np.sum(dx**2)
+    decrement_value_list = []
+    x_list = [x0]
+    for iters in range(MAXITERS):
+        dx = -grad_f(x)
+        decrement_value = decrement(dx)
+        decrement_value_list.append(decrement_value)
+        if decrement_value < TOL:
+            print("Iteration: %d, decrement: %.10f" % (iters, decrement_value))
+            break
+        t = 1
+        while not domf(x + t*dx):
             t *= beta
 
         while f(x + t*dx) > f(x) - alpha * t * decrement_value:
             t *= beta
         # print(dx)
         x += t*dx
-        # print("Iteration: %d, decrement: %f" % (iters, decrement_value))
-    return x
+        x_list.append(x)
+        # print("Iteration: %d, decrement: %.10f" % (iters, decrement_value))
+    return x, x_list, decrement_value_list
