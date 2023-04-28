b4eb6fc9a4d9d03d1e3ed2b67842c06c6610e47d
Jinyi Liu <jinyi.liu99@gmail.com>
2023-04-28 13:03:09 -0400

    quasi

--- newton.py
+++ newton.py
@@ -9,14 +9,14 @@ def newton_eq(f, grad_f, nabla_f, x0, A, b, MAXITERS=100, TOL=1e-8,alpha = 0.01,
     A, b = A, b
     m, n = A.shape
     x = x0
-    decrement = lambda dx, x: (dx.dot(nabla_f(x).dot(dx)))
+    stopping_criteria = lambda dx, x: (dx.dot(nabla_f(x).dot(dx)))/2
     
     for iters in range(MAXITERS):
         dx = np.linalg.solve(
             np.block([[nabla_f(x), A.T], [A, np.zeros((m, m))]]),
             np.block([-grad_f(x), np.zeros(m)]))
         dx = dx[:n]
-        decrement_value = decrement(dx, x)/2
+        decrement_value = stopping_criteria(dx, x)
         if decrement_value < TOL:
             break
         t = 1
@@ -108,3 +108,50 @@ def gradient_descent(f, grad_f, x0, A, b, domf, MAXITERS=100, TOL=1e-8,alpha=0.0
         obj_list.append(f(x))
         # print("Iteration: %d, decrement: %.10f" % (iters, decrement_value))
     return x_list, obj_list
+
+
+def quasi_newton(f, grad_f, nabla_f, x0, A, b, domf, MAXITERS=100, TOL=1e-8,alpha = 0.01, beta = 0.8, print_iter=False, N=1, diag_only=False, decrement_func=None):
+    MAXITERS = MAXITERS
+    TOL = TOL
+    alpha = alpha
+    beta = beta
+    f, grad_f, nabla_f, domf = f, grad_f, nabla_f, domf
+    A, b = A, b
+    m, n = A.shape
+    x = x0.copy()
+    if not decrement_func:
+        decrement = lambda dx, x: (dx.dot(nabla_f(x).dot(dx)))/2
+    else:
+        decrement = decrement_func
+    decrement_value_list = []
+    obj_list = [f(x)]
+    x_list = [x]
+    nabla = nabla_f(x)
+    for iters in range(1, MAXITERS):
+        if iters % N == 0:
+            nabla = nabla_f(x)
+            if diag_only:
+                nabla = np.diag(np.diag(nabla))
+
+        dx = np.linalg.solve(nabla, -grad_f(x))
+        dx = dx[:n]
+        decrement_value = decrement(dx, x)
+        decrement_value_list.append(decrement_value)
+        if decrement_value < TOL:
+            if print_iter:
+                print("Iteration: %d, decrement: %.10f" % (iters, decrement_value))
+            break
+        t = 1
+        while not domf(x + t*dx):
+            # print("This t is not in domain: %f" % t)
+            t *= beta
+
+        while f(x + t*dx) > f(x) - alpha * t * decrement_value:
+            t *= beta
+
+        x += t*dx
+        x_list.append(x.copy())
+        obj_list.append(f(x))
+        if print_iter:
+            print("Iteration: %d, decrement: %.10f" % (iters, decrement_value))
+    return np.array(x_list), obj_list
